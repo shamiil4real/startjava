76aa3c4d11e029f2fea5baa4196487483176cb4a
shamiil <wonderboy2k.yt@gmail.com>
2023-02-27 22:09:29 +0700

    Исправил ошибки в ДЗ

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,31 +2,19 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
     
-    private String mathOperation;
+    private String[] mathOperation;
     private double number1;
     private double number2;
     private char mathOperatore;
     private double result;
 
-    public Calculator (String mathOperation) {
-        this.mathOperation = mathOperation;
-    }
+    public double calculate(String input) {
+        String[] mathOperation = input.split(" ");
+        number1 = Double.parseDouble(mathOperation[0]);
+        number2 = Double.parseDouble(mathOperation[2]);
+        mathOperatore = mathOperation[1].charAt(0);
 
-    public void setNumber1(int number1) {
-        this.number1 = number1;
-    }
-
-    public void setNumber2(int number2) {
-        this.number2 = number2;
-    }
-
-    public void setMathOperatore(char mathOperatore) {
-        this.mathOperatore = mathOperatore;
-    }
-
-    public void calculate() {
         result = 1;
-
         switch(mathOperatore) {
             case '+':
                 result = number1 + number2;
@@ -49,11 +37,6 @@ public class Calculator {
             default :
                 System.out.println("Вы указали некорректный математический символ");
         }
-
-        if (result % 1 == 0.0) {
-            System.out.println(String.format("%.0f", result));
-        } else {
-            System.out.println(result);
-        }
+        return result;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -6,31 +6,30 @@ import java.util.Scanner;
 public class CalculatorTest {
 
     public static void main(String[] args) {
+        Calculator calc = new Calculator();
         String answer = "yes";
         Scanner scanner = new Scanner(System.in);
 
         do {
-            if (!answer.equals("no") && !answer.equals("yes")) {
-                System.out.println("Хотите продолжить вычисления? [yes/no]");
-                answer = scanner.next();
-                scanner.nextLine();
-                continue;
+            if (answer.equals("yes")) {
+                System.out.print("Введите математическое выражение: ");
+                String input = scanner.nextLine();
+
+                if (calc.calculate(input) % 1 == 0.0) {
+                    System.out.println((int)calc.calculate(input));
+                } else {
+                    System.out.println(calc.calculate(input));
+                }
             }
-
-            System.out.print("Введите математическое выражение: ");
-            String input = scanner.nextLine();
-            Calculator calc = new Calculator(input);
-            String[] mathOperation = input.split(" ");
-
-            calc.setNumber1(Integer.parseInt(mathOperation[0]));
-            calc.setNumber2(Integer.parseInt(mathOperation[2]));
-            calc.setMathOperatore(mathOperation[1].charAt(0));
-            calc.calculate();
 
             System.out.println("Хотите продолжить вычисления? [yes/no]");
             answer = scanner.next();
             scanner.nextLine();
-        } while (!answer.equals("no"));
+
+            if (answer.equals("no")) {
+                break;
+            }
+        } while (true);
 
         System.out.println("Программа завершена");
     }
