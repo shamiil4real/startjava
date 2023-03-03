22877412a19fe43e0288495bf38601b7086de2c3
shamiil <wonderboy2k.yt@gmail.com>
2023-03-03 23:45:07 +0700

    Исправил ошибки в ДЗ

--- src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
@@ -10,6 +10,7 @@ public class GuessNumberTest {
         Player player1 = new Player(scanner.next());
         System.out.print("Введите имя второго игрока: ");
         Player player2 = new Player(scanner.next());
+        
         GuessNumber game = new GuessNumber(player1, player2);
         String answer = "yes";
 
