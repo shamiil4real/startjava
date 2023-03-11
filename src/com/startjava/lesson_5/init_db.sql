8d49436a0ed4445e65ff6dc427bef7002ca7859a
shamiil <wonderboy2k.yt@gmail.com>
2023-03-11 15:10:30 +0700

    Переформатировал SQL-Файлы

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -1,110 +1,12 @@
-INSERT INTO Jaegers(
-        modelName,
-        mark,
-        weight,
-        height,
-        status,
-        origin,
-        launch,
-        kaijuKill
-    )
-VALUES (
-        'Brawler',
-        'Mark-1',
-        '77.5',
-        '7.77',
-        'destroyed',
-        'USA',
-        '30.01.15',
-        '2'
-    ),
-    (
-        'Romeo',
-        'Mark-1',
-        '77.7',
-        '7.57',
-        'destroyed',
-        'USA',
-        '15.02.15',
-        '2'
-    ),
-    (
-        'Danger',
-        'Mark-3',
-        '79.5',
-        '1.99',
-        'destroyed',
-        'USA',
-        '10.07.17',
-        '9'
-    ),
-    (
-        'Typhoon',
-        'Mark-4',
-        '76.4',
-        '1.72',
-        'destroyed',
-        'China',
-        '22.09.18',
-        '7'
-    ),
-    (
-        'Striker',
-        'Mark-5',
-        '76.2',
-        '1.85',
-        'destroyed',
-        'Australia',
-        '08.02.19',
-        '11'
-    ),
-    (
-        'Dragon',
-        'Mark-2',
-        '75.7',
-        '7.55',
-        'saved',
-        'USA',
-        '21.11.15',
-        '2'
-    ),
-    (
-        'Elephant',
-        'Mark-1',
-        '77.8',
-        '9.57',
-        'destroyed',
-        'Italy',
-        '15.02.15',
-        '1'
-    ),
-    (
-        'Lion',
-        'Mark-3',
-        '79.5',
-        '2.57',
-        'saved',
-        'USA',
-        '10.07.17',
-        '15'
-    ),
-    (
-        'Tornado',
-        'Mark-4',
-        '76.5',
-        '1.75',
-        'destroyed',
-        'Spain',
-        '24.04.18',
-        '7'
-    ),
-    (
-        'Tiger',
-        'Mark-5',
-        '79.3',
-        '1.65',
-        'saved',
-        'France',
-        '12.10.19',
-        '12'
-    );
+INSERT INTO Jaegers(modelName, mark, weight, height, status, origin, launch, kaijuKill) 
+VALUES ('Brawler', 'Mark-1', '77.5', '7.77', 'destroyed', 'USA', '30.01.15', '2'), 
+       ('Romeo', 'Mark-1', '77.7', '7.57', 'destroyed', 'USA', '15.02.15', '2'), 
+       ('Danger', 'Mark-3', '79.5', '1.99', 'destroyed', 'USA', '10.07.17', '9'), 
+       ('Typhoon', 'Mark-4', '76.4', '1.72', 'destroyed', 'China', '22.09.18', '7'), 
+       ('Striker', 'Mark-5', '76.2', '1.85', 'destroyed', 'Australia', '08.02.19', '11'), 
+       ('Dragon', 'Mark-2', '75.7', '7.55', 'saved', 'USA', '21.11.15', '2'), 
+       ('Elephant', 'Mark-1', '77.8', '9.57', 'destroyed', 'Italy', '15.02.15', '1'), 
+       ('Lion', 'Mark-3', '79.5', '2.57', 'saved', 'USA', '10.07.17', '15'), 
+       ('Tornado', 'Mark-4', '76.5', '1.75', 'destroyed', 'Spain', '24.04.18', '7'), 
+       ('Tiger', 'Mark-5', '79.3', '1.65', 'saved', 'France', '12.10.19', '12');
+
--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,52 +1,50 @@
 -- Отобразить всю таблицу
 SELECT *
-FROM Jaegers;
+  FROM Jaegers;
 
 -- Отобразить только не уничтоженных роботов
 SELECT *
-FROM Jaegers
-WHERE status = 'saved';
+  FROM Jaegers
+ WHERE status = 'saved';
 
 -- Отобразить роботов нескольких серий: Mark-1, Mark-4
 SELECT *
-FROM Jaegers
-WHERE mark IN ('Mark-1', 'Mark-4');
+  FROM Jaegers
+ WHERE mark IN ('Mark-1', 'Mark-4');
 
 -- Отобразить всех роботов, кроме Mark-1, Mark-4
 SELECT *
-FROM Jaegers
-WHERE mark NOT IN ('Mark-1', 'Mark-4');
+  FROM Jaegers
+ WHERE mark NOT IN ('Mark-1', 'Mark-4');
 
 --Отсортировать таблцу по убыванию по столбцу Mark
 SELECT *
-FROM Jaegers
-ORDER BY Mark DESC;
+  FROM Jaegers
+ ORDER BY Mark DESC;
 
 -- Отобразить информацию о самом старом роботе
 SELECT *
-FROM Jaegers
-WHERE launch = (
-        SELECT MIN(launch)
-        FROM Jaegers
-    );
+  FROM Jaegers
+ WHERE launch = 
+    (SELECT MIN(launch)
+       FROM Jaegers);
 
 -- Отобразить роботов, которые уничтожили больше всего kaiju
 SELECT *
-FROM Jaegers
-WHERE kaijuKill = (
-        SELECT MAX(kaijuKill)
-        FROM Jaegers
-    );
+  FROM Jaegers
+ WHERE kaijuKill = 
+      (SELECT MAX(kaijuKill)
+         FROM Jaegers);
 
 -- Отобразить средний вес роботов
 SELECT AVG(weight)
-FROM Jaegers;
+  FROM Jaegers;
 
 -- Увеличить на единицу количество уничтоженных kaiju у не разрушенных роботов
 UPDATE Jaegers
-SET kaijuKill = kaijuKill + 1
-WHERE status = 'saved';
+   SET kaijuKill = kaijuKill + 1
+ WHERE status = 'saved';
 
 -- Удалить разрушенных роботов 
 DELETE FROM Jaegers
-WHERE status = 'destroyed';
+ WHERE status = 'destroyed';
